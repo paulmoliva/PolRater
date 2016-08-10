ec34d34c64a8a3d7250b7d129510d4ae1b65b4c2
paulmoliva <paulmoliva@gmail.com>
2016-08-10 13:17:32 -0700

    styled and made it functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def require_login
+    redirect to root_path unless current_user
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810164500) do
+ActiveRecord::Schema.define(version: 20160810201139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160810164500) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.boolean  "admin"
   end
 
 end
